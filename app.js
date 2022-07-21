951fc9bd80a5ceae3afd65d1937198127548582b
Arman <arm.nnn04@gmail.com>
2022-07-21 19:52:05 +0600

    added some styles

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const button = document.querySelector('#card-btn');
-const cards = document.querySelectorAll('.card-title-elem');
+const cards = document.querySelectorAll('#card-title-elem');
 let minutesElem = document.querySelector('#timer-minutes');
 let secondsElem = document.querySelector('#timer-seconds');
+let cardElem = document.querySelector('.card')
+let cardBtnElem = document.querySelector('#card-btn')
 
 let seconds = 60;
 let minutes = 25;
 let Interval;
+
 
 for (let i = 0; i < cards.length; i++) {
     cards[i].onclick = function () {
@@ -15,16 +18,36 @@ for (let i = 0; i < cards.length; i++) {
             })
             cards[i].classList.add('selected')
         }
+        if(cards[i].classList.contains('short-card')){
+            clearInterval(Interval)
+            cards.forEach(card => card.style.background = 'none')
+            document.body.style.background = '#4c9195';
+            cardElem.style.background = '#5e9ca0' 
+            cards[i].style.background = '#4c9195'
+            cardBtnElem.style.color = '#4c9195'
+
+        } else if(cards[i].classList.contains('long-card')){
+            clearInterval(Interval)
+            cards.forEach(card => card.style.background = 'none')
+            document.body.style.background = '#457ca3';
+            cardElem.style.background = '#5889ac';
+            cards[i].style.background = '#457ca3'
+            cardBtnElem.style.color = '#457ca3'
+        } else{
+            cards.forEach(card => card.style.background = 'none')
+            document.body.style.background = '#bc5752';
+            cardElem.style.background = '#dd6662'
+            cards[i].style.background = '#bc5752'
+            cardBtnElem.style.color = '#bc5752'
+        }
+        
     }
 }
+
 
 button.onclick = function () {
     clearInterval(Interval)
     Interval = setInterval(decreaseTime, 1000)
-}
-
-function stopTimer() {
-    clearInterval(Interval)
 }
 
 function decreaseTime() {
